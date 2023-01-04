f5bd4a2585f27654d87601308c0bf8329c1993eb
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-01-04 12:55:43 +0530

    test 3 check

--- src/tokenize.js
+++ src/tokenize.js
@@ -6,6 +6,28 @@ const {
   isQuote,
 } = require('./identify');
 
-const tokenize = () => {};
+const tokenize = (input) => {
+  const tokens = [];  // getting an array of tokens
+  let cursor = 0; // pointer to the current character
+
+  while (cursor < input.length) {
+    const character = input[cursor];
+    if (isParenthesis(character)) {
+      tokens.push({
+        type: 'Parenthesis',
+        value: character,
+      })
+      cursor++;
+      continue;
+    }
+
+    if (isWhitespace(character)) {
+      cursor++;
+      continue;
+    }
+  }
+
+  return tokens;
+};
 
 module.exports = { tokenize };
